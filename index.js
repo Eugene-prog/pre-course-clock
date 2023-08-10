8d9e783649c316743a70aa8a195e37fdf63ddcb7
Eugene-prog <ieugene.prog@gmail.com>
2023-08-10 22:40:07 +0700

    solve task clock

---
+++ index.js
@@ -0,0 +1,36 @@
+let timerId; // переменная, которая будет хранить ID таймера
+
+
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', function () {
+    timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+});
+
+
+const stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', function () {
+    clearInterval(timerId); // останавливаем таймер
+    timerId = null;
+});
+
+
+function updateClock() {
+    const clock = document.getElementById('clock');
+    const now = new Date();
+    let hours = now.getHours();
+
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+
+    let minutes = now.getMinutes();
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    let seconds = now.getSeconds();
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+    clock.textContent = hours + ' : ' + minutes + ' : ' + seconds;
+}
